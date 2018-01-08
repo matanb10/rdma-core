aviadye/rdma_core

mlx5: Add dv for create_flow_action_esp
Certain mlx5 based hardware require special data parsing when working
with packets which went through ESP action. We would like to
avoid unaware applications from mistakenly mishandling the data,
skipping the required parsing. In order to do that, we add a new
direct verb. This direct verb is identical to the
ibv_create_flow_action_esp, but sets a vendor specific
flag that means "I expect the data to come with special metadata".
In turn, the kernel would verify that the data would indeed be
returned as expected and would fail this command otherwise.

issue: 1087238
Change-Id: I6a67875a36087f8bd732e12f3a27c41dedffa602
Signed-off-by: Matan Barak <matanb@mellanox.com>

providers/mlx5/verbs.c
@@ -2550,6 +2550,15 @@ int mlx5_modify_cq(struct ibv_cq *cq, struct ibv_modify_cq_attr *attr)
 	return ibv_cmd_modify_cq(cq, attr, &cmd, sizeof(cmd));
 }
 
+enum mlx5_ib_create_flow_action_attrs {
+	MLX5_IB_CREATE_FLOW_ACTION_FLAGS		= (1U << 12) | (1 << 0),
+};
+
+enum mlx5_ib_flow_action_flags {
+	MLX5_IB_FLOW_ACTION_FLAGS_REQUIRE_METADATA	= 1 << 0,
+	MLX5_IB_FLOW_ACTION_FLAGS_RESERVED		= 1 << 1,
+};
+
 struct ibv_flow_action *mlx5_create_flow_action_esp(struct ibv_context *ctx,
 						    const struct ibv_flow_action_esp *attr)
 {
@@ -2565,6 +2574,33 @@ struct ibv_flow_action *mlx5_create_flow_action_esp(struct ibv_context *ctx,
 	}
 
 	ret = ibv_cmd_create_flow_action_esp(ctx, attr, action, cmdb);
+	if (ret) {
+		errno = ret;
+		free(action);
+		return NULL;
+	}
+
+	return action;
+}
+
+struct ibv_flow_action *mlx5dv_create_flow_action_esp(struct ibv_context *ctx,
+						      const struct ibv_flow_action_esp *esp,
+						      struct mlx5dv_flow_action_esp *mlx5_attr)
+{
+	struct ibv_flow_action *action;
+	DECLARE_COMMAND_BUFFER_DRV(cmdb, UVERBS_OBJECT_FLOW_ACTION,
+				   UVERBS_FLOW_ACTION_ESP_CREATE, 1);
+	__u64 flags = MLX5_IB_FLOW_ACTION_FLAGS_REQUIRE_METADATA;
+	int ret;
+
+	action = calloc(1, sizeof(*action));
+	if (!action) {
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	fill_attr_uint64(cmdb, MLX5_IB_CREATE_FLOW_ACTION_FLAGS, flags);
+	ret = ibv_cmd_create_flow_action_esp(ctx, esp, action, cmdb);
 	if (ret) {
 		errno = ret;
 		free(action);

matanb10/rdma-core

providers/mlx5/verbs.c
@@ -2572,6 +2572,41 @@ struct ibv_flow_action *mlx5_create_flow_action_esp(struct ibv_context *ctx,
 	return &action->action;
 }
 
+struct ibv_flow_action *mlx5dv_create_flow_action_esp(struct ibv_context *ctx,
+						      const struct ibv_flow_action_esp_attr *esp,
+						      struct mlx5dv_flow_action_esp *mlx5_attr)
+{
+	DECLARE_COMMAND_BUFFER(attrs, UVERBS_OBJECT_FLOW_ACTION,
+			       UVERBS_METHOD_FLOW_ACTION_ESP_CREATE, 1);
+	struct verbs_flow_action *action;
+	int ret;
+
+	if (!check_comp_mask(mlx5_attr->comp_mask,
+			     MLX5DV_FLOW_ACTION_ESP_MASK_FLAGS)) {
+		errno = EOPNOTSUPP;
+		return NULL;
+	}
+
+	if (mlx5_attr->comp_mask & MLX5DV_FLOW_ACTION_ESP_MASK_FLAGS)
+		fill_attr_uint64(attrs, MLX5_IB_CREATE_FLOW_ACTION_FLAGS,
+				 mlx5_attr->action_flags);
+
+	action = calloc(1, sizeof(*action));
+	if (!action) {
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	ret = ibv_cmd_create_flow_action_esp(ctx, esp, action, attrs);
+	if (ret) {
+		errno = ret;
+		free(action);
+		return NULL;
+	}
+
+	return &action->action;
+}
+
 int mlx5_modify_flow_action_esp(struct ibv_flow_action *action,
 				const struct ibv_flow_action_esp_attr *attr)
 {