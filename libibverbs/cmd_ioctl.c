b8884ed0b9203c5bb8ef6a307d0c519fa9bf085d
Jason Gunthorpe <jgg@mellanox.com>
2018-01-08 15:06:28 -0700

    verbs: Add basic infrastructure support for the kabi ioctl
    These are macros and helper functions to help build up the ioctl
    messages from the calling commands.
    
    The scheme uses a linked list of attribute buffers with the idea that
    the driver and core stack frames will allocate these buffers on the
    stack.
    
    The ioctl wrapper pulls all of the buffers together into a linear array
    and passes that to the kernel, then scatters the answers back to where
    they came from.
    
    A collection of 'fill' helpers are provided that mirror the extraction
    helpers in the kernel. Each attribute should be used with the
    matching fill helper type.
    
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>

---
+++ libibverbs/cmd_ioctl.c
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2018 Mellanox Technologies, Ltd.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <infiniband/verbs_ioctl.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <valgrind/memcheck.h>
+
+/* Number of attrs in this and all the link'd buffers */
+unsigned int _ioctl_final_num_attrs(unsigned int num_attrs,
+				   struct ibv_command_buffer *link)
+{
+	for (; link; link = link->next)
+		num_attrs += link->next_attr - link->hdr.attrs;
+
+	return num_attrs;
+}
+
+int _ioctl_init_final_cmdb(struct ibv_command_buffer *cmd, uint16_t object_id,
+			   uint16_t method_id, struct ibv_command_buffer *next,
+			   size_t num_attrs, size_t total_attrs)
+{
+	cmd->hdr.object_id = object_id;
+	cmd->hdr.method_id = method_id;
+	cmd->next = next;
+	cmd->next_attr = cmd->hdr.attrs;
+	cmd->last_attr = cmd->hdr.attrs + total_attrs;
+
+	/* Only zero the attrs in num_attrs as the ones beyond that will be
+	 * filled during execute_ioctl
+	 */
+	memset(cmd->next_attr, 0, sizeof(*cmd->next_attr) * num_attrs);
+
+	return 0;
+}
+
+/* Linearize the link'd buffers into this one */
+static void prepare_attrs(struct ibv_command_buffer *cmd)
+{
+	struct ib_uverbs_attr *end = cmd->next_attr;
+	struct ibv_command_buffer *link;
+
+	for (link = cmd->next; link; link = link->next) {
+		struct ib_uverbs_attr *cur;
+
+		assert(cmd->hdr.object_id == link->hdr.object_id);
+		assert(cmd->hdr.method_id == link->hdr.method_id);
+
+		for (cur = link->hdr.attrs; cur != link->next_attr; cur++)
+			*end++ = *cur;
+
+		assert(end <= cmd->last_attr);
+	}
+
+	cmd->hdr.num_attrs = end - cmd->hdr.attrs;
+}
+
+/* Copy the link'd attrs back to their source and make all output buffers safe
+ * for VALGRIND */
+static void finalize_attrs(struct ibv_command_buffer *cmd)
+{
+	struct ib_uverbs_attr *end = cmd->next_attr;
+	struct ibv_command_buffer *link;
+
+	for (end = cmd->hdr.attrs; end != cmd->last_attr; end++)
+		/* MATAN: This still seems like it needs my bitmap
+		 * approach. If the underlying type is not
+		 * UVERBS_ATTR_TYPE_PTR_OUT then data is not a pointer and we
+		 * cannot call valgrind. Which is the other thing my bitmap
+		 * handled as the bit set was only done in fill_attr_out..
+		 */
+		if (end->flags & UVERBS_ATTR_F_VALID_OUTPUT) {
+			VALGRIND_MAKE_MEM_DEFINED((void *)(uintptr_t)end->data,
+						  end->len);
+		}
+
+	for (link = cmd->next; link; link = link->next) {
+		struct ib_uverbs_attr *cur;
+
+		for (cur = link->hdr.attrs; cur != link->next_attr; cur++) {
+			if (end->flags & UVERBS_ATTR_F_VALID_OUTPUT) {
+				VALGRIND_MAKE_MEM_DEFINED(
+					(void *)(uintptr_t)end->data, end->len);
+			}
+
+			/* FIXME: Should we only copy UVERBS_ATTR_F_VALID_OUTPUT
+			 * ? */
+			*cur = *end++;
+		}
+	}
+}
+
+int execute_ioctl(struct ibv_context *context, struct ibv_command_buffer *cmd)
+{
+	prepare_attrs(cmd);
+	cmd->hdr.length = sizeof(cmd->hdr) +
+		sizeof(cmd->hdr.attrs[0]) * cmd->hdr.num_attrs;
+
+	if (ioctl(context->cmd_fd, RDMA_VERBS_IOCTL, &cmd->hdr))
+		return errno;
+
+	finalize_attrs(cmd);
+
+	return 0;
+}
+
+/*
+ * Fill in the legacy driver specific structures. These all follow the
+ * 'common' structures which we do not use anymore.
+ */
+int _execute_ioctl_legacy(struct ibv_context *context,
+			  struct ibv_command_buffer *cmdb, const void *cmd,
+			  size_t cmd_common_len, size_t cmd_total_len,
+			  void *resp, size_t resp_common_len,
+			  size_t resp_total_len)
+{
+	if (cmd_common_len < cmd_total_len)
+		fill_attr_in(cmdb, UVERBS_UHW_IN,
+			     cmd_total_len - cmd_common_len,
+			     (const uint8_t *)cmd + cmd_common_len);
+	if (resp_common_len < resp_total_len)
+		fill_attr_in(cmdb, UVERBS_UHW_OUT,
+			     resp_total_len - resp_common_len,
+			     (const uint8_t *)resp + resp_common_len);
+
+	return execute_ioctl(context, cmdb);
+}
---
+++ libibverbs/verbs_ioctl.h
@@ -0,0 +1,286 @@
+/*
+ * Copyright (c) 2018 Mellanox Technologies, Ltd.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __INFINIBAND_VERBS_IOCTL_H
+#define __INFINIBAND_VERBS_IOCTL_H
+
+#include <stdint.h>
+#include <assert.h>
+#include <rdma/rdma_user_ioctl.h>
+#include <rdma/ib_user_ioctl_verbs.h>
+#include <infiniband/verbs.h>
+
+static inline uint64_t ioctl_ptr_to_u64(const void *ptr)
+{
+	if (sizeof(ptr) == sizeof(uint64_t))
+		return (uintptr_t)ptr;
+
+	/*
+	 * Some CPU architectures require sign extension when converting from
+	 * a 32 bit to 64 bit pointer.  This should match the kernel
+	 * implementation of compat_ptr() for the architecture.
+	 */
+#if defined(__tilegx__)
+	return (int64_t)(intptr_t)ptr;
+#else
+	return (uintptr_t)ptr;
+#endif
+}
+
+/*
+ * The command buffer is organized as a linked list of blocks of attributes.
+ * Each stack frame allocates its block and then calls up toward to core code
+ * which will do the ioctl. The frame that does the ioctl calls the special
+ * FINAL varient which will allocate enough space to linearize the attribute
+ * buffer for the kernel.
+ *
+ * The current range of attributes to fill is next_attr -> last_attr.
+ */
+
+struct ibv_command_buffer {
+	struct ibv_command_buffer *next;
+	struct ib_uverbs_attr *next_attr;
+	struct ib_uverbs_attr *last_attr;
+	struct ib_uverbs_ioctl_hdr hdr;
+};
+
+/*
+ * Constructing an array of ibv_command_buffer is a reasonable way to expand
+ * the VLA in hdr.attrs on the stack and also allocate some internal state in
+ * a single contiguous stack memory region. It will over-allocate the region in
+ * some cases, but this approach allows the number of elements to be dynamic,
+ * and not fixed as a compile time constant.
+ */
+#define _IOCTL_NUM_CMDB(_num_attrs)                                            \
+	((sizeof(struct ibv_command_buffer) +                                  \
+	  sizeof(struct ib_uverbs_attr) * (_num_attrs) +                       \
+	  sizeof(struct ibv_command_buffer) - 1) /                             \
+	 sizeof(struct ibv_command_buffer))
+
+unsigned int _ioctl_final_num_attrs(unsigned int num_attrs,
+				    struct ibv_command_buffer *link);
+
+/*
+ * C99 does not permit an initializer for VLAs, so this function does the init
+ * instead. It is called in the wonky way so that DELCARE_COMMAND_BUFFER can
+ * still be a 'variable', and we so we don't require C11 mode.
+ */
+int _ioctl_init_final_cmdb(struct ibv_command_buffer *cmd, uint16_t object_id,
+			   uint16_t method_id, struct ibv_command_buffer *next,
+			   size_t num_attrs, size_t total_attrs);
+
+/*
+ * Construct a 0 filled IOCTL command buffer on the stack with enough space
+ * for _num_attrs elements. This version requires _num_attrs to be a compile
+ * time constant. _link is a previous COMMAND_BUFFER in the call chain.
+ */
+#define DECLARE_COMMAND_BUFFER_LINK(_name, _object_id, _method_id, _num_attrs, \
+				    _link)                                     \
+	struct ibv_command_buffer _name[_IOCTL_NUM_CMDB(_num_attrs)] = {       \
+		{.hdr =                                                        \
+			 {                                                     \
+				 .object_id = (_object_id),                    \
+				 .method_id = (_method_id),                    \
+				 .num_attrs = _IOCTL_NUM_CMDB(_num_attrs),     \
+			 },                                                    \
+		 .next = (included),                                           \
+		 .next_attr = _name[0].hdr.attrs,                              \
+		 .base_attr = _name[0].hdr.attrs,                              \
+		 .last_attr = _name[0].hdr.attrs + _num_attrs},                \
+	}
+
+#define DECLARE_COMMAND_BUFFER(_name, _object_id, _method_id, _num_attrs)      \
+	DECLARE_COMMAND_BUFFER_LINK(_name, _object_id, _method_id, _num_attrs, \
+				    NULL)
+
+/*
+ * Construct a 0 filled IOCTL command buffer on the stack with enough space
+ * for _num_attrs elements. This version is used only by the function that
+ * calls execute_ioctl. It allocates enough storage to linearize all of the
+ * linked COMMAND_BUFFER's to be passed to the kernel.
+ */
+#define DECLARE_COMMAND_BUFFER_FINAL(_name, _object_id, _method_id,            \
+				     _num_attrs, _link)                        \
+	unsigned int __##_name##total =                                        \
+		_ioctl_final_num_cmdb(_num_attrs, _link);                      \
+	struct ibv_command_buffer _name[_IOCTL_NUM_CMDB(__##_name##total)];    \
+	int __attribute__((unused)) __##_name##dummy =                         \
+		_ioctl_init_final_cmdb(_name, _object_id, _method_id, _link,   \
+				       _num_attrs, __##_name##total)
+
+/* This many additional attributes are required to call execute_ioctl_legacy */
+#define LEGACY_ATTRS 2
+
+int execute_ioctl(struct ibv_context *context, struct ibv_command_buffer *cmd);
+
+/*
+ * execute, including the legacy driver specific attributes that use the
+ * UVERBS_UHW_IN, UVERBS_UHW_OUT
+ */
+int _execute_ioctl_legacy(struct ibv_context *context,
+			  struct ibv_command_buffer *cmdb, const void *cmd,
+			  size_t cmd_common_len, size_t cmd_total_len,
+			  void *resp, size_t resp_common_len,
+			  size_t resp_total_len);
+#define execute_ioctl_legacy(context, cmdb, cmd, cmd_size, resp, resp_size)    \
+	_execute_ioctl_legacy(context, cmdb, cmd, sizeof(*(cmd)), cmd_size,    \
+			      resp, sizeof(*(resp)), resp_size)
+
+static inline struct ib_uverbs_attr *
+_ioctl_next_attr(struct ibv_command_buffer *cmd, uint16_t attr_id)
+{
+	struct ib_uverbs_attr *attr;
+
+	assert(cmd->next_attr < cmd->last_attr);
+	attr = cmd->next_attr++;
+	attr->attr_id = attr_id;
+
+	/*
+	 * All attributes default to mandatory. Wrapper the fill_* call in
+	 * attr_optional() to make it optional.
+	 */
+	attr->flags = UVERBS_ATTR_F_MANDATORY;
+
+	return attr;
+}
+
+/* Make the attribute optional. */
+static inline struct ib_uverbs_attr *attr_optional(struct ib_uverbs_attr *attr)
+{
+	attr->flags &= ~UVERBS_ATTR_F_MANDATORY;
+	return attr;
+}
+
+/* Send attributes of kernel type UVERBS_ATTR_TYPE_IDR */
+static inline struct ib_uverbs_attr *
+fill_attr_in_obj(struct ibv_command_buffer *cmd, uint16_t attr_id, uint32_t idr)
+{
+	struct ib_uverbs_attr *attr = _ioctl_next_attr(cmd, attr_id);
+
+	attr->data = idr;
+	return attr;
+}
+
+static inline struct ib_uverbs_attr *
+fill_attr_out_obj(struct ibv_command_buffer *cmd, uint16_t attr_id)
+{
+	return fill_attr_in_obj(cmd, attr_id, 0);
+}
+
+/* Send attributes of kernel type UVERBS_ATTR_TYPE_PTR_IN */
+static inline struct ib_uverbs_attr *
+fill_attr_in(struct ibv_command_buffer *cmd, uint16_t attr_id, size_t len,
+	     const void *data)
+{
+	struct ib_uverbs_attr *attr = _ioctl_next_attr(cmd, attr_id);
+
+	assert(len <= UINT16_MAX);
+
+	attr->len = len;
+	if (len <= sizeof(uint64_t))
+		memcpy(&attr->data, data, len);
+	else
+		attr->data = ioctl_ptr_to_u64(data);
+
+	return attr;
+}
+
+#define fill_attr_in_ptr(cmd, attr_id, ptr)                                    \
+	fill_attr_in(cmd, attr_id, sizeof(*ptr), ptr)
+
+static inline struct ib_uverbs_attr *
+fill_attr_in_uint64(struct ibv_command_buffer *cmd, uint16_t attr_id,
+		    uint64_t data)
+{
+	struct ib_uverbs_attr *attr = _ioctl_next_attr(cmd, attr_id);
+
+	attr->len = sizeof(data);
+	attr->data = data;
+
+	return attr;
+}
+
+static inline struct ib_uverbs_attr *
+fill_attr_in_uint32(struct ibv_command_buffer *cmd, uint16_t attr_id,
+		    uint32_t data)
+{
+	struct ib_uverbs_attr *attr = _ioctl_next_attr(cmd, attr_id);
+
+	attr->len = sizeof(data);
+	attr->data = data;
+
+	return attr;
+}
+
+static inline struct ib_uverbs_attr *
+fill_attr_in_fd(struct ibv_command_buffer *cmd, uint16_t attr_id, int fd)
+{
+	struct ib_uverbs_attr *attr;
+
+	if (fd == -1)
+		return NULL;
+
+	attr = _ioctl_next_attr(cmd, attr_id);
+	attr->data = fd;
+	return attr;
+}
+
+static inline struct ib_uverbs_attr *
+fill_attr_out_fd(struct ibv_command_buffer *cmd, uint16_t attr_id, int fd)
+{
+	struct ib_uverbs_attr *attr;
+
+	attr = _ioctl_next_attr(cmd, attr_id);
+	attr->data = 0;
+	return attr;
+}
+
+/* Send attributes of kernel type UVERBS_ATTR_TYPE_PTR_OUT */
+static inline struct ib_uverbs_attr *
+fill_attr_out(struct ibv_command_buffer *cmd, uint16_t attr_id, size_t len,
+	      void *data)
+{
+	struct ib_uverbs_attr *attr;
+
+	attr = _ioctl_next_attr(cmd, attr_id);
+
+	assert(len <= UINT16_MAX);
+	attr->len = len;
+	attr->data = (uintptr_t)data;
+
+	return attr;
+}
+
+#define fill_attr_out_ptr(cmd, attr_id, ptr)                                 \
+	fill_attr_out(cmd, attr_id, sizeof(*(ptr)), (ptr))
+
+#endif
